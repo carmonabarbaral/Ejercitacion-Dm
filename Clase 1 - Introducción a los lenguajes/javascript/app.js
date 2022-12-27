97379aef770596c9d766b3b164578ea4464bc841
carmonabarbaral <barbaracarmona40@mail.com>
2022-12-27 10:24:10 -0300

    ejercicios clase 1

---
+++ Clase 1 - Introducción a los lenguajes/javascript/app.js
@@ -0,0 +1,50 @@
+// // El poder guardar datos en la memoria, en forma de variables
+
+// // let, const, var...
+
+// let edad;
+
+// // Inicializar una variable
+// let pais;
+
+// // Esto es declarar una variable
+// pais = "Argentina";
+
+// // Const no permite mutar a la variable
+// const apellido = "Maldonado";
+
+// console.log(edad);
+// console.log(pais);
+
+// Guardando una etiqueta html por su nombre
+//const body = document.querySelector("body");
+
+// Guardar muchos elementos utilizando la clase
+//let listaItem = document.querySelectorAll(".listaItem");
+
+//console.log(main);
+//console.log(listaItem);
+
+// Capturar cada elemento por separado en una constante y mostrar su valor en consola.
+
+// Resultado esperado: 8 variables y 8 console.log()
+
+let carrito = [];
+
+const body = document.querySelector("body");
+console.log ("body");
+
+const header = document.querySelector("header");
+console.log (header);
+
+const h1 = document.querySelector("h1");
+console.log (h1);
+
+const nav = document.querySelector("nav");
+console.log (nav);
+
+const ul = document.querySelector("ul");
+console.log (ul);
+
+const listaItem = document.querySelectorAll(".listaItem");
+console.log(listaItem);
